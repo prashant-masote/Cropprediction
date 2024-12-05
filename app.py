f5847a42dfc11c20c834e8943f04e0c6e29c17b6
prashant-masote <104124167+prashant-masote@users.noreply.github.com>
2024-12-05 20:26:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ flask_app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
 
 @flask_app.route("/")
-def hpyome():
+def home():  # Corrected function name
     return render_template("index.html")
 
-@flask_app.route("/predict", methods = ["POST"])
+@flask_app.route("/predict", methods=["POST"])
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The Predicted Crop is {}".format(prediction))
+    return render_template("index.html", prediction_text="The Predicted Crop is {}".format(prediction))
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
